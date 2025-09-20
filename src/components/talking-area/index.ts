22c182d62ba58ca6838f43dae013eacc95b296e4
Aoi <naztya2001@bk.ru>
2025-09-20 21:36:17 +0300

    fix: removed talkingArea component

--- src/components/talking-area/index.ts
+++ src/components/talking-area/index.ts
@@ -1,21 +1,21 @@
-import './talking-area.css';
-import { Block } from '../../core/block.ts';
-import template from './talking-area.hbs?raw';
-import { connect } from '../../utils/connect.ts';
+// import './talking-area.css';
+// import { Block } from '../../core/block.ts';
+// import template from './talking-area.hbs?raw';
+// import { connect } from '../../utils/connect.ts';
 
-export class TalkingAreaBase extends Block {
-    render() {
-        return this._compile(template, this.props);
-    }
-}
+// export class TalkingAreaBase extends Block {
+//     render() {
+//         return this._compile(template, this.props);
+//     }
+// }
 
-type BlockClass = new (props?: Record<string, unknown>) => Block;
-const mapStateToProps = (props: Record<string, [BlockClass, { [key: string]: Block | Object }]>) => {
-    return {
-        user: props.chat[1].talkingAreaInfo.user,
-        messages: props.chat[1].talkingAreaInfo.messages,
-        isMock: props.chat[1].talkingAreaInfo.isMock
-    };
-};
+// type BlockClass = new (props?: Record<string, unknown>) => Block;
+// const mapStateToProps = (props: Record<string, [BlockClass, { [key: string]: Block | Object }]>) => {
+//     return {
+//         user: props.chat[1].talkingAreaInfo.user,
+//         messages: props.chat[1].talkingAreaInfo.messages,
+//         isMock: props.chat[1].talkingAreaInfo.isMock
+//     };
+// };
 
-export const TalkingArea = connect(mapStateToProps)(TalkingAreaBase);
+// export const TalkingArea = connect(mapStateToProps)(TalkingAreaBase);
